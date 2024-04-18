b6d0d558c5aa469e7e42df36ecdffca766d4db3b
Sampul-CodeMine <studysampul@gmail.com>
2024-04-18 19:13:08 +0100

    Solution to Task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -19,9 +19,9 @@ def user_login() -> Tuple[str, int]:
     """
     email = request.form.get('email')
     passwd = request.form.get('password')
-    if email is None or len(email) == 0:
+    if email is None or len(email.strip()) == 0:
         return jsonify({'error': 'email missing'}), 400
-    if passwd is None or len(passwd) == 0:
+    if passwd is None or len(passwd.strip()) == 0:
         return jsonify({'error': 'password missing'}), 400
 
     try:
@@ -36,4 +36,4 @@ def user_login() -> Tuple[str, int]:
         data = jsonify(users_obj[0].to_json())
         data.set_cookie(os.environ.get('SESSION_NAME', sess_id))
         return data
-    return jsonify({'error': 'wrong password'})
+    return jsonify({'error': 'wrong password'}), 401
