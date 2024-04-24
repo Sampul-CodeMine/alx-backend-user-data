1fda2bb1945b291a61955eedbddd2e3e8d670c09
Sampul-CodeMine <studysampul@gmail.com>
2024-04-24 18:18:49 +0100

    Solution to Task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -68,3 +68,24 @@ class DB:
                 if getattr(user, field) == value:
                     return user
         raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """This is a method to update a user data in the  database
+
+        Args:
+            user_id(int): the user with id for which to update data
+            kwargs(dict): keyworded argument with which to update user record
+
+        Returns:
+            Nothing
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for field, value in kwargs.items():
+                if hasattr(user, field):
+                    setattr(user, field, value)
+                else:
+                    raise ValueError()
+            self._session.commit()
+        except NoResultFound:
+            raise ValueError()
