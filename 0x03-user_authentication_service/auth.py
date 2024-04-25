a1a091391322267103b9bf70cf779ad96f550486
Sampul-CodeMine <studysampul@gmail.com>
2024-04-25 13:31:29 +0100

    Solution to Task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,3 +42,20 @@ class Auth:
             self._db.add_user(email, _hash_password(password))  # add user
         else:
             raise ValueError(f'User {email} already exists')  # raise an error
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """This is a method to validate user Login
+
+        Args:
+            email(str, required): This is the email of the user to login
+            password(str, required): The plaintext password of user to login
+
+        Returns:
+            (bool): True if login validated else False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
