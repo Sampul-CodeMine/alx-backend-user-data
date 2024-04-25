f9439288f66b7b6401a1c0db5e68b60d025b3b36
Sampul-CodeMine <studysampul@gmail.com>
2024-04-25 14:03:00 +0100

    Solution to Task 9 and Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if user:
                 sess_id = _generate_uuid()
-                self._db.update_user(user.id, session_id=sess_id)
+                user.session_id = sess_id
                 return sess_id
             return None
         except NoResultFound:
